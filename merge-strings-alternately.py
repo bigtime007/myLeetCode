fa902f25a46e5f3828b286dc9b8d102455e4bc2b
Kevin <62586828+bigtime007@users.noreply.github.com>
2024-03-17 17:50:08 -0500

    Update merge-strings-alternately.py

--- merge-strings-alternately.py
+++ merge-strings-alternately.py
@@ -14,3 +14,25 @@ class Solution:
         result.append(word2[minLength:])
     
         return ''.join(result)
+
+
+class Solution2:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # Pointer Solution
+        pointer1, pointer2 = 0, 0
+        result = []
+        
+        # Iterate until one of the strings is exhausted
+        while pointer1 < len(word1) and pointer2 < len(word2):
+            result.append(word1[pointer1])
+            result.append(word2[pointer2])
+            pointer1 += 1
+            pointer2 += 1
+        
+        # Correctly append the remaining characters of the longer string
+        if pointer1 < len(word1):
+            result.append(word1[pointer1:])
+        if pointer2 < len(word2):
+            result.append(word2[pointer2:])
+        
+        return ''.join(result)
